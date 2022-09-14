b09d203ea294f5bae07f11490ab0ae85c551f57f
Kevin Csiffary <kevin.csiffary@gmail.com>
2022-09-14 22:22:11 +0000

    created helloWorldStyle.cpp

---
+++ helloWorldStyle.cpp
@@ -0,0 +1,12 @@
+// Copyright (c) 2022 Kevin Csiffary All rights reserved.
+// .
+// Created by: Kevin Csiffary
+// Date: Sep. 14, 2022
+// This program prints "Hello, World!" and has comments
+
+#include <iostream>
+
+int main() {
+    std::cout << "Hello World!\n";
+    std::cout << "This one has comments\n";
+}
